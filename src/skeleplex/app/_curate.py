b47744d86b56480dca81828f52c486b99fcb2aa5
MalteMederacke <malte.mederacke@gmx.de>
2026-04-25 13:04:55 +0200

    make point size dependent on dataset span

--- src/skeleplex/app/_curate.py
+++ src/skeleplex/app/_curate.py
@@ -503,7 +503,10 @@ def make_split_edge_widget(viewer):
         viewer._viewer._backend.reslice_all()
 
     split_edge_widget.split_pos.changed.connect(preview_split)
-    split_edge_widget.point_visual, split_edge_widget.point_store = viewer.add_points()
+    point_size = np.max(np.max(viewer.data.node_coordinates) * 0.01, 50)
+    split_edge_widget.point_visual, split_edge_widget.point_store = viewer.add_points(
+        point_size=point_size
+        )
     split_edge_widget.point_visual.appearance.visible = False
 
     return split_edge_widget
